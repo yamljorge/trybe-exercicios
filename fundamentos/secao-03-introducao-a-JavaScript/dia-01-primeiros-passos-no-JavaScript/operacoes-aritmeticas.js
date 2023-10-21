130553552c808579a0b2d5134ee11b0355943436
yamljorge <yamlirajorge@gmail.com>
2023-10-21 16:55:16 -0300

    adiciona operacoes aritmeticas em javascript

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-01-primeiros-passos-no-JavaScript/operacoes-aritmeticas.js
@@ -0,0 +1,27 @@
+let salary = 3502;
+let bonus = 2000;
+let vacations = 1000;
+let taxes = 782;
+
+console.log(salary * 12);
+console.log(salary * 12 + bonus + vacations - taxes);
+console.log(salary / 4);
+console.log(salary % 2);
+console.log(salary += 10); 
+console.log(salary -= 1);
+console.log(10 + 22);
+console.log(100 - 40);
+console.log(4 * 7);
+console.log(40 / 5);
+
+const base = 5;
+const height = 8;
+const area = base * height;
+
+console.log(area);
+
+const perimeter = 5 + 5 + 8 + 8;
+
+console.log(perimeter);
+
+
