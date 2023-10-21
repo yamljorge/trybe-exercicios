38b85540869de1608c86ee4a133b064201724062
yamljorge <yamlirajorge@gmail.com>
2023-10-21 19:23:34 -0300

    adiciona operadores lógicos

--- fundamentos/secao-03-introducao-a-JavaScript/dia-01-primeiros-passos-no-JavaScript/operadores -logicos.js
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-01-primeiros-passos-no-JavaScript/operadores -logicos.js
@@ -41,3 +41,58 @@ if (currentHour >= 22) {
 console.log(message);
 
 //Operador OR
+
+const principalDrink = 'café';
+const alternativeDrink = 'morango';
+
+if (principalDrink === 'café' || alternativeDrink === 'suco de laranja') {
+  console.log("Agradeço por me atender :D");
+} else {
+  console.log("Ei, não foi isso que eu pedi!");
+}
+
+console.log(true || true); 
+console.log(true || false); 
+console.log(false || true); 
+console.log(false || false); 
+
+const goToSP = false;
+const goToGO = true;
+
+if ((goToSP === true && goToGO === false) || (goToSP === false && goToGO === true)) {
+  console.log('Eu fui para apenas um dos lugares');
+} else {
+  console.log('Eu fui para os dois, ou para nenhum.');
+}
+
+
+
+//Operador NOT
+
+console.log((2 + 2) === 4);
+console.log(!(2 + 2) === 4);
+
+//Strings
+
+let charmander = "Melhor Pokémon inicial";
+
+console.log(charmander);
+console.log(!charmander);
+
+//Números
+
+console.log(42);
+console.log(0);
+
+console.log(!42);
+console.log(!0); // O número 0 tem o valor "falsy" no JavaScript. Logo, seu oposto é true.
+
+//Valores Nulos
+
+console.log(null);
+console.log(!null);
+
+//Valores Indefinidos
+
+console.log(undefined);
+console.log(!undefined);
