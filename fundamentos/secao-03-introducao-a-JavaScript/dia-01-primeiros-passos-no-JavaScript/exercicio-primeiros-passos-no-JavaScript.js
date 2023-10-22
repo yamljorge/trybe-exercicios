b42e5b9b6ededd3e365a6970f00f8f9a0db87439
yamljorge <yamlirajorge@gmail.com>
2023-10-22 17:04:36 -0300

    resolucao do exercio numero 3

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-01-primeiros-passos-no-JavaScript/exercicio-primeiros-passos-no-JavaScript.js
@@ -0,0 +1,39 @@
+//Exercício 1
+
+let num1 = 2;
+let num2 = 3;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+//Exercício 2
+
+let num3 = 3;
+let num4 = 4;
+
+if (num3 > num4) {
+  console.log(num3 + " é maior que " + num);
+} else if (num3 === num4) {
+  console.log("Os números são iguais.");
+} else {
+  console.log(num4 + " é maior que " + num3);
+};
+
+//Exercício 3
+
+let num5 = 0;
+let num6 = 1;
+let num7 = 0;
+
+if (num5 > num6 &&  num5 > num7) {
+  console.log('O maior número é ' + num5);
+} else if (num6 > num5 &&  num6 > num7) {
+  console.log('O maior número é ' + num6);
+} else if (num7 > num5 &&  num7 > num6) {
+  console.log('O maior número é ' + num7);
+} else {
+  console.log('IHHHH, tem números iguais!');
+};
