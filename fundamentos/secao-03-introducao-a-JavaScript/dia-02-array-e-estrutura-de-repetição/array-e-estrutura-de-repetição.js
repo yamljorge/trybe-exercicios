123abd47c1f7b1bc3761583b7c871a80536041c3
yamljorge <yamlirajorge@gmail.com>
2023-10-23 15:34:06 -0300

    adiciona template literals

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-02-array-e-estrutura-de-repetição/array-e-estrutura-de-repetição.js
@@ -0,0 +1,15 @@
+//Array e estrutura de repetição
+
+const produto = 'Iphone 15';
+const preço = 12895;
+const desconto = 33;
+
+const mensagem =
+`Produto: ${produto}
+Preço: ${preço}
+Desconto: ${desconto}%
+Preço com desconto: ${(preço - (preço * (desconto / 100))).toFixed(2)}`;
+
+console.log(mensagem);
+
+
