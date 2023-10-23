2d8737ab60cb8e8228a86f08847829eea365c864
yamljorge <yamlirajorge@gmail.com>
2023-10-23 14:51:03 -0300

    adiona switcase

--- fundamentos/secao-03-introducao-a-JavaScript/dia-01-primeiros-passos-no-JavaScript/exercicio-primeiros-passos-no-JavaScript.js
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-01-primeiros-passos-no-JavaScript/exercicio-primeiros-passos-no-JavaScript.js
@@ -60,5 +60,72 @@ if(allAngulo){
 
 //Exercício 5
 
+let chessPiece = 'rei';
 
+switch (chessPiece.toLowerCase()) {
+  case 'rei':
+    console.log('Rei -> Uma casa para qualquer direção.');
+    break;
+  case 'bispo':
+    console.log('Bispo -> Diagonais.');
+    break;
+  case 'rainha':
+    console.log('Rainha -> Diagonal, horizontal e vertical.');
+    break;
+  case 'cavalo':
+    console.log('Cavalo -> "L" pode pular sobre as peças.');
+    break;
+  case 'torre':
+    console.log('Torre -> Horizontal e vertical.');
+    break;
+  case 'peão':
+    console.log("Peão -> Uma casa para frente, no primeiro movimento podem ser duas casas.");
+    break;
+  default:
+    console.log('Erro, peça inválida!');
+};
 
+// Exercício 6
+
+const nu1 = 18;
+const nu2 = 25;
+const nu3 = 77;
+
+let Par = false;
+
+if ((nu1 / 2 === 0 || nu2 / 2 === 0 || nu3 / 2 === 0)) { Par = true;
+};
+console.log(Par);
+
+//Exercicio 7
+
+let aliquotaINSS;
+let aliquotaIR;
+
+let salarioBruto = 1000.00;
+
+if (salarioBruto <= 1556.94) {
+  aliquotaINSS = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+  aliquotaINSS = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82) {
+  aliquotaINSS = salarioBruto * 0.11;
+} else {
+  aliquotaINSS = 570.88;
+};
+
+let baseSalarial = salarioBruto - aliquotaINSS;
+
+if (baseSalarial <= 1903.98) {
+  aliquotaIR = 0;
+} else if (baseSalarial <= 2826.65) {
+  aliquotaIR = (baseSalarial * 0.075) - 142.80;
+} else if (baseSalarial <= 3751.05) {
+  aliquotaIR = (baseSalarial * 0.15) - 354.80;
+} else if (baseSalarial <= 4664.68) {
+  aliquotaIR = (baseSalarial * 0.225) - 636.13;
+} else {
+  aliquotaIR = (baseSalarial * 0.275) - 869.36;
+};
+
+console.log("Salário: R$" + (baseSalarial - aliquotaIR));
